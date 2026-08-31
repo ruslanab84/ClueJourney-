b62e49de10b23907ed0264d0b3afca611041f145
Ruslan Abdulov <rusikabdulov@gmail.com>
2026-08-31 20:04:30 +0400

    Initial Commit

---
+++ ClueJourney/ClueJourneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClueJourneyApp.swift
+//  ClueJourney
+//
+//  Created by Ruslan Abdulov on 31.08.26.
+//
+
+import SwiftUI
+
+@main
+struct ClueJourneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClueJourney/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ClueJourney
+//
+//  Created by Ruslan Abdulov on 31.08.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
